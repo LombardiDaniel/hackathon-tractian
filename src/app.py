ab1fc795dd48f59e1a452ab0c96b77e81a3182ef
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2024-10-28 15:29:42 -0300

    running on 5555

--- src/app.py
+++ src/app.py
@@ -1,22 +1,37 @@
 import os
+from collections import OrderedDict
 from uuid import uuid4
 
 import openai
 from flask import Flask, jsonify, request
 
+from main import (
+    formatar_resultado,
+    genprompt,
+    remove_duplicates_ordered,
+    remove_negative_values,
+)
 from services import MongoDBDocumentStore
 
 app = Flask(__name__)
+MONGODB_ATLAS_CLUSTER_URI = os.getenv("MONGODB_ATLAS_CLUSTER_URI")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-MONGODB_ATLAS_CLUSTER_URI = os.getenv(
-    "MONGODB_ATLAS_CLUSTER_URI",
-)
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+openai.api_key = OPENAI_API_KEY
+
+DB_NAME = "hackathon-tractian"
+COLLECTION_NAME = "hackathon-tractian-vectorstores"
+
 
 STORE_SERVICE = MongoDBDocumentStore(
-    MONGODB_ATLAS_CLUSTER_URI, "hackathon-tractian", "hackathon-tractian-vectorstores"
+    MONGODB_ATLAS_CLUSTER_URI, DB_NAME, COLLECTION_NAME
 )
 
+NR12_ID = "671fc44758dc9867a50d752a"
+W22_ID = "671fa85458dc9867a50d751e"
+
+asked = []
 # {
 #     "message": ""
 # }
@@ -28,27 +43,58 @@ STORE_SERVICE = MongoDBDocumentStore(
 @app.route("/ask", methods=["POST"])
 def ask():
     data = request.json
+    question = data["message"]
 
-    system_prompt = genprompt(assuntos, resultados)
+    results: list[dict] = STORE_SERVICE.vector_search([NR12_ID], question)
+    pages: list[int] = []
+    for res in results:
+        page_num = res["metadata"]["pageNum"]
+        docId = res["metadata"]["docId"]
+        text = res["text"]
+        # search_results.append(formatar_resultado(page_num, docId, text))
+        pages.append(page_num - 1)
+        pages.append(page_num)
+        pages.append(page_num + 1)
+
+    pages = remove_duplicates_ordered(pages)
+    pages = remove_negative_values(pages)
+
+    chunks = []
+    for page in pages:
+        chunks.extend(STORE_SERVICE.get_doc_chunks(NR12_ID, page))
+
+    search_results: list[str] = []
+    for chunk in chunks:
+        # print(chunk["text"])
+        page_num = chunk["metadata"]["pageNum"]
+        docId = chunk["metadata"]["docId"]
+        text = chunk["text"]
+        search_results.append(formatar_resultado(page_num, docId, text))
+
+    system_prompt = genprompt(["segurança no trabalho"], search_results)
+
+    asked.append(
+        {"role": "user", "content": question},
+    )
+    messages = [_ for _ in asked]
+    if len(asked) >= 10:
+        asked = asked[-10:]
+
+    messages.append({"role": "system", "content": system_prompt})
+
+    # print(messages)
 
     reply = openai.chat.completions.create(
         model="gpt-4o",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {
-                "role": "user",
-                "content": data.get("question", ""),
-            },
-        ],
+        messages=messages,
+    )
+    rep_string = reply.choices[0].message.content
+    asked.append(
+        {"role": "assistant", "content": rep_string},
     )
 
-    formatted_results = []
-    for res in reply.choices[0].message.content.split("\n"):
-        if res.startswith("["):
-            formatted_results.append(res.strip())
-
-    return jsonify({"results": formatted_results})
+    return jsonify({"reply": rep_string})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5555, debug=True)
